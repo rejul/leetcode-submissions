2ecd0498e912fc753209f79e3c4e369b34e6cf8e
Rejul Raghu <rejul.reghu@gmail.com>
2026-06-14 19:52:41 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+
+        if(s.length!=t.length){
+            return false
+        }
+        let lookup = new Map<string,number>();
+
+        for(var c of s){
+            lookup.set(c,(lookup.get(c)||0)+1);
+       }
+        for( c of t){
+            lookup.set(c,(lookup.get(c)||0)-1)
+       }
+        for(let num of lookup.values()){
+            if(num!=0){
+                return false
+
+            }
+        }
+        return true
+
+
+        
+    }
+
+}
